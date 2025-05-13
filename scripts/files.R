7793a716216af0f3b0c07acc3b55285d7837b2e0
Ulas Isildak <isildak.ulas@gmail.com>
2025-05-13 13:56:15 +0200

    final cleaning for publishing3

--- scripts/files.R
+++
@@ -1,6 +0,0 @@
-mydf = readRDS('/scratch/shire/data/biobank/ukbb_immunosenescence/mt-aging/data/processed/ukb678748_subset_df_all_final.rds')
-mymetadf = readRDS('/scratch/shire/data/biobank/ukbb_immunosenescence/mt-aging/data/processed/ukb678748_subset_df_all_columns.rds')
-
-figure_out_dir = '/scratch/shire/data/biobank/ukbb_immunosenescence/mt-aging/results/figures'
-table_out_dir = '/scratch/shire/data/biobank/ukbb_immunosenescence/mt-aging/results/tables'
-project_dir = '/scratch/shire/data/biobank/ukbb_immunosenescence/mt-aging/'
